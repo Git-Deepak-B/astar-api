2cc7d302f90e65b86b623899adeec221fb5256f4
Deepak <deepak@softops.in>
2021-12-29 21:43:58 +0530

    Initial Commit

---
+++ src/main/java/sg/wavescan/astarapi/AstarApiApplication.java
@@ -0,0 +1,13 @@
+package sg.wavescan.astarapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AstarApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AstarApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sg/wavescan/astarapi/AstarApiApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.wavescan.astarapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AstarApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
